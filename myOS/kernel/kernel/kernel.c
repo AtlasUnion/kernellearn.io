679fbbe6751864e19f2ef90e849981d83feb77a1
Ryan Ruan <zruan@bu.edu>
2019-07-30 13:35:47 -0400

    Style

--- myOS/kernel/kernel/kernel.c
+++ myOS/kernel/kernel/kernel.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <kernel/tty.h>
+#include <kernel/multiboot.h>
 
-void kernel_main(void) {
-    terminal_initialize();
-    printf("Hello kernel World!\n");
+void kernel_main(multiboot_info_t *mbd, unsigned int magic_num)
+{
+	terminal_initialize();
+	printf("Hello World of Kernel! Welcome to my OS!\n");
+	printf("The magic number is %X\n", magic_num);
 }
